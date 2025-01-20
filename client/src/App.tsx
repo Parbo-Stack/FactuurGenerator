7fd4860b9167458fd4e540454221395c751647bc
parbo23 <13299961-parbo23@users.noreply.replit.com>
2025-01-20 20:24:48 +0000

    Agent query: Is the page loading without the useContext error now?
    Wrap application with I18nextProvider for internationalization.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/fb17c21d-6ec2-4d34-8f38-68d718ab9474/b8751fa4-9582-4871-941b-06dbf68b9630.jpg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,8 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
 import InvoiceGenerator from "@/pages/invoice-generator";
-import "./lib/i18n";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./lib/i18n";
 
 function Router() {
   return (
@@ -17,10 +18,12 @@ function Router() {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router />
-      <Toaster />
-    </QueryClientProvider>
+    <I18nextProvider i18n={i18n}>
+      <QueryClientProvider client={queryClient}>
+        <Router />
+        <Toaster />
+      </QueryClientProvider>
+    </I18nextProvider>
   );
 }
 
